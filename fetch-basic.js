590a21026c492e0800dacac2a470dad4fe6e9ac3
jctrotter <trotterj@oregonstate.edu>
2019-07-28 17:08:14 -0700

    base fetching functionality complete

---
+++ fetch-basic.js
@@ -0,0 +1,46 @@
+var https = require('https');
+var fs = require('fs')
+
+var result;
+
+https.get('https://api.scryfall.com/cards/random?q=t%3Abasic', (resp) => {
+	let data = '';
+	resp.on('data', (chunk) => {
+		data += chunk;
+	});
+
+	resp.on('end', () => {
+		result = JSON.parse(data);
+		let img_url = result.image_uris.art_crop;
+		//write('images/'+result.oracle_id+'.json', data)
+		fs.writeFile('images/'+result.oracle_id+'.json', data, 'utf-8', function(err){
+			if (err) throw err
+			console.log("Card info saved.")
+		})
+		get_img(img_url)
+	});
+
+}).on("error", (err) => {
+	console.log("Error: " + err.message);
+});
+
+function get_img(img_url){
+	https.get(img_url, (resp) => {
+		let img_data = '';
+		resp.setEncoding('binary');
+
+		resp.on('data', (chunk) => {
+			img_data += chunk;
+		});
+		
+		resp.on('end', () => {
+			fs.writeFile('images/'+result.oracle_id+'.png', img_data, 'binary', function(err){
+				if (err) throw err
+				console.log('Image saved.')
+			})
+		});
+
+	}).on("error", (err) => {
+		console.log("Error: " + err.message);
+	})
+};
